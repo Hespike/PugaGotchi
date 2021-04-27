15776a0af6dd4897c4beadedf1012d3ee913260c
Sárdi András <sardi.andras@gmail.com>
2021-04-27 18:59:33 +0200

    Update PugaGotchi.java

--- PugaGotchi.java
+++ PugaGotchi.java
@@ -4,6 +4,8 @@ public class PugaGotchi {
     public static void main(String[] args) {
         int kor = 0;
         int penz = 1500;
+        int elsoUtasitas = 0;
+        int masodikUtasitas = 0;
 
         Mopsz mopszi = new Mopsz();
         mopszi.getJollakotsag();
@@ -11,29 +13,48 @@ public class PugaGotchi {
         mopszi.getKedv();
         mopszi.getEnergia();
         mopszi.getRendetlenseg();
-        
+
 
        while(kor < 100 && penz < 100000){
            kor++;
-        System.out.println("Az aktuális kör:" + kor + ". Jelenleg a jóllakotság ennyi:" + mopszi.jollakotsag );
-        Scanner utasitas = new Scanner(System.in);
-        System.out.println("Lehetséges utasítások:");
-        System.out.println("1: Étel vásárlása");
-        System.out.println("2: Életcsomag vásárlása");
-        System.out.println("3: Állatorvos meglátogatása");
-        System.out.println("4: Maximalizáló ital vásárlása");
-        System.out.println("5: Irány játszani!");
-        System.out.println("6: Irány dolgozni!");
+        System.out.println(" Aktuális kör: " + kor + " Pénzmennyiség: " + penz + ". Jóllakotság: " + mopszi.jollakotsag + " Egészség: " + mopszi.egeszseg + " Kedv: " + mopszi.kedv + " Energia: " + mopszi.energia + " Rendetlenség: " + mopszi.rendetlenseg + "." );
 
-        System.out.println("Írj be egy utasítást!");
+           System.out.println("Mit szeretnél csinálni? Írd be a megfelelő számot!");
+           System.out.println("1: Étel vásárlása");
+           System.out.println("2: Életcsomag vásárlása");
+           System.out.println("3: Állatorvos meglátogatása");
+           System.out.println("4: Maximalizáló ital vásárlása");
+           System.out.println("5: Irány játszani!");
+           System.out.println("6: Irány dolgozni!");
 
-          int aktUtasitas = utasitas.nextInt();  // Read user input
-        System.out.println("Az általad választott utasítás: " + aktUtasitas);  // Output user input
+           Scanner sc = new Scanner(System.in);
+           String utasitas=sc.next();
+           elsoUtasitas = Integer.parseInt(utasitas);
 
-    Mopsz jatekosKarakter = new Mopsz();
+           switch(elsoUtasitas){
+              case 1:
+                  System.out.println("Mit szeretnél enni?");
+                  System.out.println("1: Jutalomfalatka (jóllakotság: +10, pénz: -4, energia: -3");
+                  System.out.println("2: Száraz kutyatáp (jóllakotság: +20, pénz: -8, energia: -3");
+                  System.out.println("3: Csont (jóllakotság: +23, pénz: -10, energia: -3");
+                  System.out.println("4: Házikoszt (jóllakotság: +15, pénz: -6, energia: -3");
+                  System.out.println("5: Víz (jóllakotság: +7, pénz: -3, energia: -3");
+                  System.out.println("6: Konzerv kutyaeledel (jóllakotság: +30, pénz: -13, energia: -3");
+                  System.out.println("7: Kutya energiaital (jóllakotság: +20, pénz: -20, energia: +12");
+                  sc = new Scanner(System.in);
+                  utasitas = sc.next();
+                  masodikUtasitas = Integer.parseInt(utasitas);
+                  mopszi.eszik(masodikUtasitas, penz);
+                  }
+
+           }
+
+
+
+        System.out.println("Az általad választott utasítás: " + elsoUtasitas + " és " + masodikUtasitas);
 
        }
 
     }
 
-}
+
