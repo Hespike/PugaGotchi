75284682b0a382d659428ec089fda28fc52190c1
Sárdi András <sardi.andras@gmail.com>
2021-04-30 14:54:08 +0200

    Update PugaGotchi.java

--- PugaGotchi.java
+++ PugaGotchi.java
@@ -17,7 +17,7 @@ public class PugaGotchi {
             kor++;
             System.out.println("Aktuális kör: " + kor + ". Pénzmennyiség: " + mopszi.penz + ". Jóllakotság: " + mopszi.jollakotsag + ". Egészség: " + mopszi.egeszseg + ". Kedv: " + mopszi.kedv + ". Energia: " + mopszi.energia + ". Rendetlenség: " + mopszi.rendetlenseg + ".");
 
-            System.out.println("Mit szeretnél csinálni? Írd be a megfelelő számot!\r\n1: Étel vásárlása\r\n2: Életcsomag vásárlása\r\n3: Állatorvos meglátogatása\r\n4: Maximalizáló ital vásárlása\r\n5: Irány játszani!\r\n6: Irány dolgozni!");
+            System.out.println("Mit szeretnél csinálni? Írd be a megfelelő számot!\r\n1: Étel vásárlása\r\n2: Egészségesebbé tenni a kutyust\r\n3: Irány játszani!\r\n6: Irány dolgozni!");
 
 
             Scanner sc = new Scanner(System.in);
@@ -26,7 +26,7 @@ public class PugaGotchi {
 
             switch (elsoUtasitas) {
                 case 1: //eszik
-                    System.out.println("Mit szeretnél enni?\r\n1: Jutalomfalatka  (jóllakotság: +10, pénz: -4, energia: -3)\r\n2: Száraz kutyatáp (jóllakotság: +20, pénz: -8, energia: -3)\r\n3: Csont (jóllakotság: +23, pénz: -10, energia: -3)\r\n4: Házikoszt (jóllakotság: +15, pénz: -6, energia: -3)\r\n5: Víz (jóllakotság: +7, pénz: -3, energia: -3)\r\n6: Konzerv kutyaeledel (jóllakotság: +30, pénz: -13, energia: -3)\r\n7: Kutya energiaital (jóllakotság: +20, pénz: -20, energia: +12)");
+                    System.out.println("Mit szeretnél enni adni a kutyusnak?\r\n1: Jutalomfalatka  (jóllakotság: +10, pénz: -4, energia: -3)\r\n2: Száraz kutyatáp (jóllakotság: +20, pénz: -8, energia: -3)\r\n3: Csont (jóllakotság: +23, pénz: -10, energia: -3)\r\n4: Házikoszt (jóllakotság: +15, pénz: -6, energia: -3)\r\n5: Víz (jóllakotság: +7, pénz: -3, energia: -3)\r\n6: Konzerv kutyaeledel (jóllakotság: +30, pénz: -13, energia: -3)\r\n7: Kutya energiaital (jóllakotság: +20, pénz: -20, energia: +12)");
                     sc = new Scanner(System.in);
                     utasitas = sc.next();
                     masodikUtasitas = Integer.parseInt(utasitas);
@@ -34,13 +34,19 @@ public class PugaGotchi {
                     break;
 
                 case 2: //egészség fenntartás
-                    System.out.println("Mit szeretnél enni?\r\n1: Kis életcsomag vásárlása (egészség: +25, pénz: -6)\r\n2: Nagy életcsomag vásárlása (egészség: +50, pénz: -25)\r\n3: Irány a doktor! (egészség: az értéke 100%-ra vált, pénz: -25)\r\n4: Maximalizáló ital vásárlása (Minden tulajdonság 100%-ra áll, pénz: -100)");
+                    System.out.println("Mit szeretnél tenni a kutyus egészségért?\r\n1: Kis életcsomag vásárlása (egészség: +25, pénz: -6)\r\n2: Nagy életcsomag vásárlása (egészség: +50, pénz: -25)\r\n3: Irány a doktor! (egészség: az értéke 100%-ra vált, pénz: -25)\r\n4: Maximalizáló ital vásárlása (Minden tulajdonság 100%-ra áll, pénz: -100)");
                     sc = new Scanner(System.in);
                     utasitas = sc.next();
                     masodikUtasitas = Integer.parseInt(utasitas);
                     mopszi.egeszsegFenntart(masodikUtasitas);
                     break;
                 case 3:
+                    System.out.println("Mit szeretnétek játszani?\r\n1: Sétáljunk (energia: -10, egészség: -4)\r\n2: Labdázzunk (energia: -18,egészség: -(0-9) közötti érték, rendetlenség: -5)\r\n3: Irány a kutyaiskola (pénz: -10, energia: -20, egészség: -5, rendetlenség: -50)\r\n4: Játszunk a parkban (energia: -10, egészség: -10, rendetlenség - 10)");
+                    sc = new Scanner(System.in);
+                    utasitas = sc.next();
+                    masodikUtasitas = Integer.parseInt(utasitas);
+                    mopszi.jatszik(masodikUtasitas);
+
                     break;
                 case 4:
                     break;
